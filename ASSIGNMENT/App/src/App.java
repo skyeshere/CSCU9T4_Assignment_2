92aff8be763087837f94e7f336de798cce44ec72
skye <skyepluh@gmail.com>
2024-04-07 21:17:01 +0100

    im silly and shouldn't use Scanner to try parse a csv file :3 welp

--- ASSIGNMENT/App/src/App.java
+++ ASSIGNMENT/App/src/App.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
+import java.time.LocalTime;
 
 public class App {
+    /**
+     * 
+     * @param args, args[0] = file directory to csv file
+     */
     public static void main(String[] args)
     {
         parseCSV(args[0]);    
@@ -9,15 +15,6 @@ public class App {
 
     public static void parseCSV(String directory)
     {
-        try
-        {
-            Scanner scanner = new Scanner(new File(directory));
-            System.out.println(directory);
-        } 
-        catch (Exception e) 
-        {
-            System.err.println(e + "file not found, exiting");
-            System.exit(0);
-        }
+        ArrayList<Project> project_list = new ArrayList<>();
     }
 }
