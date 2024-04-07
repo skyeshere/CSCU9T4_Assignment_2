324f0fb14a2770a492e144cc27295dcfb20bf350
skye <skyepluh@gmail.com>
2024-04-07 15:17:50 +0100

    added file reader for filepath that SHOULD be in the commandline args

--- ASSIGNMENT/App/src/App.java
+++ ASSIGNMENT/App/src/App.java
@@ -2,8 +2,22 @@ import java.util.Scanner;
 import java.io.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println(args[0]);
+    public static void main(String[] args)
+    {
+        parseCSV(args[0]);    
+    }
+
+    public static void parseCSV(String directory)
+    {
+        try
+        {
+            Scanner scanner = new Scanner(new File(directory));
+            System.out.println(directory);
+        } 
+        catch (Exception e) 
+        {
+            System.err.println(e + "file not found, exiting");
+            System.exit(0);
+        }
     }
 }
