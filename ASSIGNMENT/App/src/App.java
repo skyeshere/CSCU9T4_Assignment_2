7fa2bbeee1f19de34ea70e77fe0d11f556b43064
skye <skyepluh@gmail.com>
2024-04-07 13:29:17 +0100

    added my personal directory for command line arguments

--- ASSIGNMENT/App/src/App.java
+++ ASSIGNMENT/App/src/App.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println(args[0]);
     }
 }
